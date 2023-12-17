28b18252b1480e99645b0f444b409fe2bd23920b
dhrurana96 <153836146+dhrurana96@users.noreply.github.com>
2023-12-17 13:14:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    var x = document.getElementById("myLinks");
+  // If the links are currently displayed, hide them; otherwise, show them
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+  
+  }
